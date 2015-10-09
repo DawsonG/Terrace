1bf89404979dcbd6999106973e8d206f51ce8377
Dawson Goodell <dawson@westlakeinteractive.com>
2015-10-09 16:42:53 -0400

    fix routes

--- routes.js
+++ routes.js
@@ -6,13 +6,13 @@ module.exports = function(app) {
   });
 
   app.get('/admin', function(req, res) {
-    res.sendFile(__dirname + "/admin/index.html");
+    res.sendFile(__dirname + "/admin/dist/index.html");
   });
 
-  app.get('/tlibs/*', function(req, res) {
+  app.get('/admin/js/*', function(req, res) {
     // Validate that we are logged in.
 
     // Send whatever file is needed.
-    res.sendFile(__dirname + "/admin/libs/" + req.params[0]);
+    res.sendFile(__dirname + "/admin/dist/build/" + req.params[0]);
   });
 };
