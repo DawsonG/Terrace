ca3e141f2e872b17e3c6c5bb674fc2beb093340a
Dawson Goodell <dawson@westlakeinteractive.com>
2015-10-09 16:21:48 -0400

    trying out some fancy gulping

--- routes.js
+++ routes.js
@@ -6,13 +6,16 @@ module.exports = function(app) {
   });
 
   app.get('/admin', function(req, res) {
-    res.sendFile(__dirname + "/admin/index.html");
+    // Validate that the user is logged in.
+
+    // Send the default file.
+    res.sendFile(__dirname + "/admin/dist/index.html");
   });
 
-  app.get('/tlibs/*', function(req, res) {
+  app.get(['/admin/js/*', '/admin/css/*'], function(req, res) {
     // Validate that we are logged in.
 
     // Send whatever file is needed.
-    res.sendFile(__dirname + "/admin/libs/" + req.params[0]);
+    res.sendFile(__dirname + "/admin/dist/build/" + req.params[0]);
   });
 };
