10b2f97a598b0fc5a7f66a7d4423df894d6973fa
cherylafitz <cherylafitzgerald@gmail.com>
2015-10-09 13:28:38 -0700

    react config

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+
+var SRC_DIR = './admin/assets';
+var DEST_DIR = './static';
+
+gulp.task('browserify', function(){
+  return browserify(SRC_DIR + '/js/main.js',{debug:true})
+    .transform('reactify')
+    .bundle()
+    .on('error',function(err){
+      console.log('error',err.stack);
+      console.log('-- THERE IS AN ERROR SEE ABOVE --');
+      this.emit('end');
+    })
+    .pipe(source('main.js'))
+    .pipe(gulp.dest(DEST_DIR + '/js'));
+});
+
+gulp.task('copy', function(){
+  return gulp.src([SRC_DIR + '/**/*.*','!' + SRC_DIR + '/js/**/*.*'])
+  .pipe(gulp.dest(DEST_DIR));
+});
+
+gulp.task('build',['browserify', 'copy']);
+
+gulp.task('watch',function(){
+  return gulp.watch(SRC_DIR + '/**/*.*', ['build']);
+});
+
+gulp.task('default',['build','watch']);
+
+
+
+
+
