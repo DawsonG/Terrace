6b550cda1374a595337d7b2f30ffc25a4311bd69
Dawson Goodell <dawson@westlakeinteractive.com>
2015-10-19 20:22:19 -0400

    empty objects to bring in line with master

--- api/posts.js
+++ api/posts.js
@@ -1,10 +1,9 @@
 exports.getById = function(req, res) {
-
+  res.json({});
 };
 
 exports.getOne = function(req, res) {
-  console.log(req);
-
+  req.json({});
   //req.db.collection('posts').findOne( , function(err, result) {
   //  if (err) res.send(err);
   //  res.json(result)
