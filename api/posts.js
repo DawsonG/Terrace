8f1771a1ddd2196969193bea9297c6d60e08c03c
Dawson Goodell <dawson@westlakeinteractive.com>
2015-10-19 20:23:02 -0400

    whoops

--- api/posts.js
+++ api/posts.js
@@ -3,7 +3,7 @@ exports.getById = function(req, res) {
 };
 
 exports.getOne = function(req, res) {
-  req.json({});
+  res.json({});
   //req.db.collection('posts').findOne( , function(err, result) {
   //  if (err) res.send(err);
   //  res.json(result)
